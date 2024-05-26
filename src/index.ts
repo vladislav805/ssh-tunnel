6a9882b0f95bc4e172eec748333114462874b71e
Vladislav Veluga <vladislav805@yandex.com>
2024-05-26 13:38:38 +0300

    Add  and

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ export interface ITunnelOptions {
     remoteHost: string;
     remotePort: number;
     sshKeyPath?: string;
+    noCommands?: boolean;
+    reverse?: boolean;
 }
 
 export interface ITunnel {
@@ -18,14 +20,19 @@ const log = debug('ssh-tunnel');
 
 export function createTunnel(options: ITunnelOptions) {
     return new Promise<ITunnel>((resolve, reject) => {
-        const args = [
-            '-R', `0.0.0.0:${options.remotePort}:127.0.0.1:${options.localPort}`,
-            `${options.remoteUser}@${options.remoteHost}`,
-        ];
+        const args: string[] = [];
 
         if (options.sshKeyPath !== undefined) {
             args.push('-i', options.sshKeyPath);
         }
+
+        args.push(options.reverse ? '-R' : '-L', `0.0.0.0:${options.remotePort}:127.0.0.1:${options.localPort}`);
+
+        if (options.noCommands) {
+            args.push('-N');
+        }
+
+        args.push(`${options.remoteUser}@${options.remoteHost}`);
 
         const proc = spawn('ssh', args);
 
@@ -40,12 +47,14 @@ export function createTunnel(options: ITunnelOptions) {
             log('[tunnel] Connecting to SSH...');
         });
 
-        proc.stdout.on('data', () => {
+        proc.stdout.on('data', (chunk) => {
             log(`[tunnel] Tunnel created: ${options.remoteHost}:${options.remotePort}`);
 
             resolve(tunnel);
         });
 
-        proc.on('exit', () => reject);
+        proc.on('exit', code => {
+            reject(code);
+        });
     });
 }
